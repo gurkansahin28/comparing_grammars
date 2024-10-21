6c36524238ca50c297f68f59d83717d61812c6d1
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-21 18:05:40 +0300

    Create kwargs.dart

---
+++ dart/kwargs.dart
@@ -0,0 +1,12 @@
+void info(Map map) {
+  print(map);
+}
+
+void main(List<dynamic> args) {
+  Map<String, String> personals = {
+    'name': 'gurkan',
+    'age': '55',
+    'location': 'Turkey'
+  };
+  info(personals);
+}
